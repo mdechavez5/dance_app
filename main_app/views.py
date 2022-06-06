8fb45e79fd5ba52759d7215ae58dfc55f18bb884
mdechavez5 <marcdechavez@gmail.com>
2022-06-06 09:40:14 -0400

    Added Home & About Urls, Views, Templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
