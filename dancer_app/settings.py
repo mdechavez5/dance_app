1377705b63c55599961f9fb274618fad758bca33
mdechavez5 <marcdechavez@gmail.com>
2022-06-10 13:36:40 -0400

    Heroku debug

--- dancer_app/settings.py
+++ dancer_app/settings.py
@@ -17,7 +17,8 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR,'main_app/static']
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'main_app/static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
