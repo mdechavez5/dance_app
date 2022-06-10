8ef99da16565f6c2cb453b712c806277417db8e0
mdechavez5 <marcdechavez@gmail.com>
2022-06-10 15:46:53 -0400

    Adding crispy-forms-bulma

--- dancer_app/settings.py
+++ dancer_app/settings.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import os
 import django_heroku
+from crispy_forms_bulma.settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
